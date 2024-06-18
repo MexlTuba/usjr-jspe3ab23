dbecebe1d5fea8755f44cb713540b81dcc104a48
Mexl Delver Tuba <90206212+MexlTuba@users.noreply.github.com>
2024-06-18 18:02:28 +0800

    models,controller,routes

---
+++ app/Models/Program.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    public function college()
+    {
+        return $this->belongsTo(College::class, 'progcollid');
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'progdeptid');
+    }
+}
---
+++ app/Models/Student.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    public function college()
+    {
+        return $this->belongsTo(College::class, 'studcollid');
+    }
+
+    public function program()
+    {
+        return $this->belongsTo(Program::class, 'studprogid');
+    }
+}
